de8050f292b56f161e3fdc5cabe8d5180a9d9818
phucdh410 <phucdh410@gmail.com>
2024-02-29 08:43:10 +0700

    Update

--- src/common/layouts/CMainLayout/CSidebar/CNavigation/context/index.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-export const SidebarContext = createContext({
-  groupOpen: "",
-  setGroupOpen: (groupId: string) => {},
-  childOpen: "",
-  setChildOpen: (childId: string) => {},
-});
--- src/common/layouts/CMainLayout/CSidebar/CNavigation/index.tsx
+++ src/common/layouts/CMainLayout/CSidebar/CNavigation/index.tsx
@@ -1,31 +1,16 @@
-import { useState } from "react";
-
 import { Stack } from "@mui/material";
 
 import { SIDEBAR } from "@/common/constants/sidebar";
 
 import { CGroup } from "./CGroup";
-import { SidebarContext } from "./context";
 
 export const CNavigation = () => {
-  //#region Data
-  const [groupOpen, setGroupOpen] = useState<string>("");
-  const [childOpen, setChildOpen] = useState<string>("");
-  //#endregion
-
-  //#region Event
-  //#endregion
-
   //#region Render
   return (
     <Stack>
-      <SidebarContext.Provider
-        value={{ groupOpen, setGroupOpen, childOpen, setChildOpen }}
-      >
-        {SIDEBAR.map((group, index) => (
-          <CGroup key={group.id} data={group} />
-        ))}
-      </SidebarContext.Provider>
+      {SIDEBAR.map((group, index) => (
+        <CGroup key={group.id} data={group} />
+      ))}
     </Stack>
   );
   //#endregion
